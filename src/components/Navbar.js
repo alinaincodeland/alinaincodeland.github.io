e53ef65255ee7284a47f68d6ada245d2a4839383
alinaincodeland <alinacuznetov@gmail.com>
2023-03-26 16:14:05 +0200

    Added navbar and styling.

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+import "./navbar.css";
+
+export default function App() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar expand="lg" sticky className="navbar">
+      <MDBContainer fluid className="navbar-container">
+        <MDBNavbarBrand href="#">alina in codeland</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav
+            right
+            fullWidth={false}
+            className="mb-2 mb-lg-0 navbar-items"
+          >
+            <MDBNavbarItem>
+              <MDBNavbarLink aria-current="page" href="#home">
+                home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#skills">skills</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#projects">projects</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#contact">contact</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
