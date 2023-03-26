c286c997ac61a7a4d401a94850ff709cdbdb68cb
alinaincodeland <alinacuznetov@gmail.com>
2023-03-26 20:15:35 +0200

    Added projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 import "./projects.css";
+import DCIconnect from "../assets/DCIconnect.png";
+import EdMundo from "../assets/EdMundo.png";
+import Meme from "../assets/meme-generator.png";
+import HelloFresh from "../assets/HelloFresh.png";
+import WeatherApp from "../assets/WeatherApp.png";
 
 export default function Projects() {
   return (
@@ -10,19 +15,35 @@ export default function Projects() {
           <h1 className="project-title">My projects</h1>
         </MDBRow>
         <MDBRow>
-          <MDBCol size="md">
-            Project 1<video src=""></video>
+          <MDBCol md={6}>
+            <div className="project-card">
+              <img src={EdMundo} alt="EdMundo project" />
+            </div>
+
+            <div>EdMundo</div>
           </MDBCol>
-          <MDBCol size="md">
-            Project 2<video src=""></video>
+          <MDBCol md={6}>
+            <div className="project-card">
+              <img src={DCIconnect} alt="DCIConnect project" />
+            </div>
+
+            <div>DCIConnect</div>
           </MDBCol>
         </MDBRow>
         <MDBRow className="project-container-row">
-          <MDBCol size="md">
-            Project 3 <video src=""></video>
+          <MDBCol md={6}>
+            <div className="project-card">
+              <img src={HelloFresh} alt="EdMundo project" />
+            </div>
+
+            <div>HelloFresh Clone</div>
           </MDBCol>
-          <MDBCol size="md">
-            Project 4<video src=""></video>
+          <MDBCol md={6}>
+            <div className="project-card">
+              <img src={WeatherApp} alt="Weather app project" />
+            </div>
+
+            <div>Weather app</div>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
