36bb7e44f7a6c4c44a99d4f4cba3eb8c319014da
alinaincodeland <alinacuznetov@gmail.com>
2024-07-21 13:40:36 +0200

    Fix hero.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,13 +14,13 @@ const Hero = () => {
           <MDBCol md="7" className="hero-text">
             <h1 className="hero-title">Alina Cuznetov</h1>
             <h1 className="hero-subtitle"> Frontend Engineer</h1>
-            <p className="hero-text">
+            <p className="hero-description">
               I'm a creative frontend engineer based in Berlin with over two
               years of experience. Currently at sofatutor, I excel in developing
               and deploying A/B tests, maintaining unit and integration tests. I
               am enthusiastic about applying my current knowledge while
-              remaining committed to learning new technologies. My aspiration is
-              to join a forward-thinking company that values mentorship and
+              remaining committed to learning new technologies. I am passionate
+              by forward-thinking companies that values mentorship and
               diversity.
             </p>
 
