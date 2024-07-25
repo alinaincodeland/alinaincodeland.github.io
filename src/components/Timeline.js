898e1168cedf736608475541752404c73358a752
alinaincodeland <alinacuznetov@gmail.com>
2024-07-25 15:16:25 +0200

    Add content to limeline.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,41 +8,50 @@ export default function Timeline() {
   return (
     <VerticalTimeline>
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        className="vertical-timeline-element"
         contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="01.03 - present"
+        date="03.24 - present"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
       >
-        <h3 className="timeline-title">Frontend Engineer</h3>
-        <h4 className="timeline-subtitle">Berlin, DE</h4>
+        <h4 className="timeline-title">Frontend Engineer</h4>
         <p>
-          Creative Direction, User Experience, Visual Design, Project
-          Management, Team Leading
+          Developed competency in Ruby, HAML and BEM by running A/B tests to
+          improve user retention and implementing unit and integration tests.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2010 - 2011"
-        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        className="vertical-timeline-element"
+        date="06.23 - 10.23"
       >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
+        <h4 className="vertical-timeline-element-title">Frontend Developer</h4>
+        <p>
+          Developed competency in Angular, TypeScript, and Storybook by
+          successfully deploying frontend features and collaborating with design
+          and frontend teams using Git/Gitlab, Jira, and Figma.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element"
+        date="04.23 - 05.23"
+      >
+        <h4 className="vertical-timeline-element-title">Frontend Developer</h4>
+        <p>
+          Substituted Vuetify 2 framework on an internal business platform with
+          Tailwind CSS custom components.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element"
+        date="02.21 - 03.22"
+      >
+        <h4 className="vertical-timeline-element-title">
+          People and Office Manager
         </h4>
         <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online
-          Marketing
+          Coordinated the design and development of a new corporate
+          website.Planned and executed company events, perks & benefits,
+          on/off-boarding, social media campaigns and office tasks.
         </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2008 - 2010"
-        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-      >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-        <p>User Experience, Visual Design</p>
       </VerticalTimelineElement>
     </VerticalTimeline>
   );
