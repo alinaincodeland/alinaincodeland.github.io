2c9899fdf8a61c9ff3421b36c89dc77ac8ccdc75
alinaincodeland <alinacuznetov@gmail.com>
2024-08-28 10:02:24 +0200

    Revert changes.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
       <MDBContainer className="hero-section">
         <MDBRow>
           <MDBCol md="7" className="hero-text">
-            <h1 className="hero-title">Alina Cuznetov</h1>
+            <h1 className="hero-title">Alina Cuznețov</h1>
             <h1 className="hero-subtitle"> Frontend Engineer</h1>
             <p className="hero-description">
               Hey there! I'm a frontend engineer with a creative twist, calling
@@ -19,15 +19,20 @@ const Hero = () => {
               that value mentorship and diversity. Let's build the future
               together!
             </p>
-
-            <a
-              className="hero-demo-button"
-              href="https://drive.google.com/file/d/1ps-7QyuIQyHoTnP2k1dj1RBBUNk5WUoZ/view?usp=drive_link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              See my CV
-            </a>
+            <div class="container-button">
+              <a
+                href="https://drive.google.com/file/d/1ps-7QyuIQyHoTnP2k1dj1RBBUNk5WUoZ/view?usp=drive_link"
+                class="button type--A hero-demo-button"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div class="button__line"></div>
+                <div class="button__line"></div>
+                <span class="button__text">CHECK MY CV</span>
+                <div class="button__drow1"></div>
+                <div class="button__drow2"></div>
+              </a>
+            </div>
           </MDBCol>
           <MDBCol md="5" className="hero-container-image">
             <img src={Profile} alt="woman-avatar" className="hero-image-main" />
